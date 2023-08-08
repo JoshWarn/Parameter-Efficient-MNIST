4088bee14faef102a1482bcffe4d8c8103fa3f1c
JoshWarn <70070682+JoshWarn@users.noreply.github.com>
2023-08-08 16:31:24 -0400

    Updated load_and_test_model with new model.

--- load_and_test_model.py
+++ load_and_test_model.py
@@ -4,32 +4,34 @@ from tensorflow_addons.optimizers import AdamW
 from keras.layers import Input, Conv2D, SeparableConv2D, MaxPooling2D, GlobalAveragePooling2D, Dense, Dropout
 import numpy as np
 
-def s_conv_block(kernels, l, padding='valid'):
-    l = SeparableConv2D(kernels, (3, 3), depth_multiplier=1, padding=padding, activation=tf.keras.layers.LeakyReLU(alpha=0.2))(l)
-    l = Dropout(rate=0.1)(l)
+
+def s_conv_block(kernels, l, padding='valid', dropout=True):
+    l = SeparableConv2D(kernels, (3, 3), depth_multiplier=1, padding=padding, activation=tf.keras.layers.LeakyReLU(alpha=0.2), kernel_initializer=tf.keras.initializers.he_normal)(l)
+    if dropout:
+        l = Dropout(rate=0.05)(l)
     return l
 
 
 def model_function():
     inputs = Input((28, 28, 1), dtype=np.float32)
+
     l = tf.keras.layers.Rescaling(1./255., offset=0.0)(inputs)
-    l = Conv2D(7, (3, 3), activation=tf.keras.layers.LeakyReLU(alpha=0.2))(l)
+    l = Conv2D(6, (3, 3), activation=tf.keras.layers.LeakyReLU(alpha=0.2), kernel_initializer=tf.keras.initializers.he_normal, padding='same')(l)
     l = MaxPooling2D((2, 2))(l)
-    l = Dropout(rate=0.1)(l)
+    l = s_conv_block(5, l)
+    l = s_conv_block(9, l)
     l = s_conv_block(10, l)
-    l = s_conv_block(10, l)
-    l = s_conv_block(11, l)
-    l = s_conv_block(12, l)
+    l = s_conv_block(8, l, dropout=False)
     l = GlobalAveragePooling2D()(l)
     l = Dense(10, activation='softmax')(l)
     model = Model(inputs, l)
-    op = AdamW(weight_decay=1e-6, learning_rate=0.01)
+    op = AdamW(weight_decay=1e-6, learning_rate=1e-2)
     model.compile(loss='sparse_categorical_crossentropy', optimizer=op, metrics=['accuracy'])
 
-    trainableParams = int(np.sum([np.prod(v.get_shape()) for v in model.trainable_weights]))
-    nonTrainableParams = int(np.sum([np.prod(v.get_shape()) for v in model.non_trainable_weights]))
-    totalParams = int(trainableParams + nonTrainableParams)
-    # print(f"Trainable: {trainableParams}, Non-Trainable: {nonTrainableParams}, Total: {totalParams}")
+    trainableParams = np.sum([np.prod(v.get_shape()) for v in model.trainable_weights])
+    nonTrainableParams = np.sum([np.prod(v.get_shape()) for v in model.non_trainable_weights])
+    totalParams = trainableParams + nonTrainableParams
+    print(f"Trainable: {trainableParams}, Non-Trainable: {nonTrainableParams}, Total: {totalParams}")
     return model, totalParams
 
 
@@ -38,5 +40,5 @@ x_test = np.expand_dims(x_test, axis=-1)
 
 model, params = model_function()
 print(params, print(len(x_test)))
-model.load_weights("wei_997_0.9906.h5")
+model.load_weights("697.0_mnist_006_1691302318_0.9903.h5")
 model.evaluate(x_test, y_test)
